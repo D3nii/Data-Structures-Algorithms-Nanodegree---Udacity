05eed46490281804ab6edd24253d6425697c2535
Muhammad Danyal Jamil Chohan <56127751+D3nii@users.noreply.github.com>
2020-07-16 22:47:56 +0300

    Update problem_1.py

--- Projects/P1/Submission/problem_1.py
+++ Projects/P1/Submission/problem_1.py
@@ -1,88 +1,35 @@
-class Node:
-    def __init__ (self, val):
-        self.data = val
-        self.next = None
-        self.prev = None
+from collections import OrderedDict
 
-class LinkedList:
-    def __init__ (self):
-        self.head = None
-        self.tail = None
-    
-    def prepend (self, val):
-        node = self.head
-        
-        if node is None:
-            self.head = Node(val)
-            self.tail = self.head
-            self.tail.prev = self.head
-            return
-        
-        self.head = Node(val)
-        self.head.next = node
-        node.prev = self.head
-
-    def remove (self, val):
-        node = self.head
-        
-        if node is None: return
-        
-        if node.data == val:
-            self.head = self.head.next
-            return
-        
-        while (node.next):
-            if node.next.data == val:
-                node.next = node.next.next
-                return
-            
-            node = node.next
-        
-    def pop (self):
-        node = self.tail
-        self.tail = node.prev
-        self.tail.next = None
-        
-        return node.data
-    
-    def size (self):
-        node = self.head
-        answer = 0
-        
-        while node:
-            answer += 1
-            node = node.next
-            
-        return answer
-        
 class LRU_Cache(object):
-
     def __init__(self, capacity):
         # Initialize class variables
-        self.list = LinkedList()
-        self.dict = dict()
+        self.data = OrderedDict()
         self.cap = capacity
 
     def get(self, key):
         # Retrieve item from provided key. Return -1 if nonexistent.
-        answer = self.dict.get(key)
+        try:
+            val = self.data.pop(key)
+            self.data[key] = val
+            return val
         
-        if answer:
-            return answer
-        return -1
+        except KeyError:
+            return -1
 
     def set(self, key, value):
         # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
         
-        if self.dict.get(key): return
-        
-        if self.cap == self.list.size():
-            old = self.list.pop()
-            del self.dict[old]
-            
-        self.list.remove(key)
-        self.list.prepend(key)
-        self.dict[key] = value
+        if self.capacity == 0: return
+
+        if key in self.data:
+            val = self.data.pop(key)
+            self.data[key] = val
+        else:
+            if len(self.data) < self.capacity:
+                self.data[key] = val
+            else:
+                self.data.popitem(last = False)
+                self.data[key] = val
 
 # Normal Case:
 our_cache = LRU_Cache(5)
@@ -91,31 +38,33 @@ our_cache.set(2, 2)
 our_cache.set(3, 3)
 our_cache.set(4, 4)
 
-print(our_cache.get(1))
-# returns 1
-print(our_cache.get(2))
-# returns 2
-print(our_cache.get(9))
-# returns -1 because 9 is not present in the cache
+print(our_cache.get(1)) # returns 1
+print(our_cache.get(2)) # returns 2
+print(our_cache.get(9)) # returns -1 because 9 is not present in the cache
 
 our_cache.set(5, 5)
 our_cache.set(6, 6)
 
-print(our_cache.get(3))
-# returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+print(our_cache.get(3)) # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
 
-# Edge Case:
+our_cache=LRU_Cache(3)
+our_cache.set(1,1)
+our_cache.set(2,2)
+our_cache.set(3,3)
+our_cache.set(4,4)
+our_cache.get(4) # Expected Value = 4
+our_cache.get(1) # Expected Value = -1
+our_cache.set(2,4)
+print(our_cache.get(2)) # Expected Value = 4 Your Output = 2
+
+# Edge Cases:
 our_cache = LRU_Cache(2)
 our_cache.set(1, 1)
 our_cache.set(2, 2)
 our_cache.set(1, 10)
-print(our_cache.get(1))
-# should return 10
-print(our_cache.get(2))
-# should return 2
+print(our_cache.get(1)) # should return 10
+print(our_cache.get(2)) # should return 2
 
 our_cache = LRU_Cache(0)
-our_cache.set(1, 1)
-# should print some message like "Can't perform operations on 0 capacity cache"
-print(our_cache.get(1))
-# should return -1
+our_cache.set(1, 1) # should print some message like "Can't perform operations on 0 capacity cache"
+print(our_cache.get(1)) # should return -1
